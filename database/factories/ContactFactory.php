398a3b33944b85dee1ecfed66c440f232d3fd681
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-10-29 00:02:15 +0100

    Initial commit

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Contact;
+use App\User;
+use Faker\Generator as Faker;
+
+$factory->define(Contact::class, function (Faker $faker) {
+    return [
+        //
+        'user_id' => factory(User::class),
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'birthday' => '04/09/1993',
+        'company' => $faker->company,
+    ];
+});
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+Route::middleware('auth:api')->group(function(){
+
+    Route::get('/contacts', 'ContactsController@index');
+    Route::post('/contacts', 'ContactsController@store');
+    Route::get('/contacts/{contact}', 'ContactsController@show');
+    Route::patch('/contacts/{contact}', 'ContactsController@update');
+    Route::delete('/contacts/{contact}', 'ContactsController@destroy');
+
+    Route::get('birthdays', 'BirthdaysController@index');
+    Route::post('search', 'SearchController@index');
+});
+
+
+
