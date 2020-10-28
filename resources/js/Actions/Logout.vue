57396c897e003c2e04af4a6774cabe6117e3e341
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-10-29 00:23:30 +0100

    Some final things

---
+++ resources/js/Actions/Logout.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Logout",
+        create(){
+            axios.post('/logout', {})
+                .finally(err => {
+                    window.location = '/login';
+                })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
